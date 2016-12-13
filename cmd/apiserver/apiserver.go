ba6d93ca3298d61964feaeb317c56156c00c27b9
huangjiasingle <449264675@qq.com>
2016-12-13 19:57:46 +0800

    first commit

---
+++ cmd/apiserver/apiserver.go
@@ -0,0 +1,16 @@
+package apiserver
+
+import (
+	"flag"
+	"minipaas/pkg/util/config"
+)
+
+var (
+	kubeconfig = flag.String("kubeconfig", "config", "--kubeconfig=kubeconfig file path")
+	miniconfig = flag.String("config", "config.json", "--config=config file path")
+)
+
+func main() {
+	flag.Parse()
+	config.Parse(miniconfig)
+}
